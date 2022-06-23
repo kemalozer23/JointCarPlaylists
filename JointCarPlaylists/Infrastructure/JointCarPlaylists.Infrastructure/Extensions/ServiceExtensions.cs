12c916cba7bef8a86b9de87412ec832c32b211c8
kemalozer23 <89297908+kemalozer23@users.noreply.github.com>
2022-06-23 17:55:14 +0300

    finished loggermanger

---
+++ JointCarPlaylists/Infrastructure/JointCarPlaylists.Infrastructure/Extensions/ServiceExtensions.cs
@@ -0,0 +1,34 @@
+﻿using JointCarPlaylists.Application.Abstractions.Logger;
+using JointCarPlaylists.Infrastructure.Services.Logger;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JointCarPlaylists.Infrastructure.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                 .AllowAnyHeader());
+            });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+
+    }
+}
